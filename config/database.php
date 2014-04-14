ed470acd4c5cd539be06b67aebe1ec4132cf8bc6
Oscar Vegas <oscarmagedon@gmail.com>
2014-04-14 12:06:45 -0400

    ignoring

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => 'gamma99',
+		'password' => 'gamma',
 		'database' => 'totalparlay',
 		'prefix' => '',
 	);
