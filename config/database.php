6b79cc8025fa5deb5524412a0b9083cad393670f
Oscar Vegas <oscarmagedon@gmail.com>
2014-04-14 12:15:49 -0400

    ignoring oo

--- config/database.php
+++ config/database.php
@@ -7,7 +7,7 @@ class DATABASE_CONFIG {
 		'host' => 'localhost',
 		'login' => 'root',
 		'password' => 'gamma',
-		'database' => 'totalparlay',
+		'database' => 'totalpafdgrlay',
 		'prefix' => '',
 	);
 	
