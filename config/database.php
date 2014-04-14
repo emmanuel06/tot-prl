52d4eff8336280f8dc8883da86a38a5048d4b023
Oscar Vegas <oscarmagedon@gmail.com>
2014-04-14 11:40:54 -0400

    epale seedbox

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => 'gammaxs',
+		'password' => 'gamma',
 		'database' => 'totalparlay',
 		'prefix' => '',
 	);
