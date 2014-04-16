2285012c90aae0de642cc723f89d07e8efbb0418
Oscar Vegas <oscarmagedon@gmail.com>
2014-04-16 16:47:47 -0400

    probando

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => 'gamma',
+		'password' => 'gamsdfma',
 		'database' => 'totalparlay',
 		'prefix' => '',
 	);
